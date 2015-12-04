897991f69cd429d7722acdd4057bf8d50a5d9db6
Maciej Gryka <maciej.gryka@gmail.com>
2015-12-04 19:56:36 +0100

    upped the version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='destimator',
-    version='0.0.0.dev3',
+    version='0.0.1',
 
     description='A metadata-saving proxy for scikit-learn etimators.',
     long_description=long_description,
