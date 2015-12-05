c098f295a555f53653e3159b1d2203c0670b115b
Maciej Gryka <maciej.gryka@gmail.com>
2015-12-05 11:10:47 +0100

    improved setupy.py

--- destimator/__init__.py
+++ destimator/__init__.py
@@ -1,1 +1,14 @@
 from .described_estimator import DescribedEstimator
+
+
+__version__ = '0.0.4'
+
+__title__ = 'destimator'
+__description__ = 'A metadata-saving proxy for scikit-learn etimators.'
+__uri__ = 'https://github.com/rainforestapp/destimator'
+
+__author__ = 'Maciej Gryka'
+__email__ = 'maciej@rainforestqa.com'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2015 RainforestQA'
--- setup.py
+++ setup.py
@@ -1,96 +1,95 @@
+import codecs
+import os
+import re
+
 from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
 
-here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
-long_description = (
-    'destimator makes it easy to store trained `scikit-learn` estimators '
-    'together with their metadata (training data, package versions, '
-    'performance numbers etc.). This makes it much safer to store '
-    'already-trained classifiers/regressors and allows for better '
-    'reproducibility.'
-)
+###################################################################
 
-setup(
-    name='destimator',
-    version='0.0.4.dev0',
+NAME = 'destimator'
+PACKAGES = find_packages(where='.')
+META_PATH = os.path.join('destimator', '__init__.py')
+KEYWORDS = ['scikit-learn', 'machine-learning', 'data', 'science']
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Natural Language :: English',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+]
+INSTALL_REQUIRES = [
+    'requests>=2.8.1',
+    'numpy>=1.10.1',
+    'scipy>=0.16.0',
+    'scikit-learn>=0.17',
+]
 
-    description='A metadata-saving proxy for scikit-learn etimators.',
-    long_description=long_description,
-
-    # The project's main homepage.
-    url='https://github.com/rainforestapp/destimator',
-
-    # Author details
-    author='Maciej Gryka',
-    author_email='maciej@rainforstqa.com',
-
-    # Choose your license
-    license='MIT',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
+EXTRAS_REQUIRE = {
+    'dev': [
+        'check-manifest'
+        'twine',
     ],
-
-    # What does your project relate to?
-    keywords='scikit-learn machine-learning data science',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-
-    install_requires=[
-        'requests>=2.8.1',
-        'numpy>=1.10.1',
-        'scipy>=0.16.0',
-        'scikit-learn>=0.17',
+    'test': [
+        'pytest',
+        'flake8',
+        'pytest-cov',
+        'tox',
     ],
+}
 
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={
-        'dev': [
-            'check-manifest'
-            'twine',
-        ],
-        'test': [
-            'pytest',
-            'flake8',
-            'pytest-cov',
-            'tox',
-        ],
-    },
+###################################################################
 
-    # # To provide executable scripts, use entry points in preference to the
-    # # "scripts" keyword. Entry points provide cross-platform support and allow
-    # # pip to create the appropriate form of executable for the target platform.
-    # entry_points={
-    #     'console_scripts': [
-    #         'sample=sample:main',
-    #     ],
-    # },
-)
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    """
+    Build an absolute path from *parts* and and return the contents of the
+    resulting file.  Assume UTF-8 encoding.
+    """
+    with codecs.open(os.path.join(HERE, *parts), 'rb', 'utf-8') as f:
+        return f.read()
+
+
+META_FILE = read(META_PATH)
+
+
+def find_meta(meta):
+    """
+    Extract __*meta*__ from META_FILE.
+    """
+    meta_match = re.search(
+        r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta),
+        META_FILE, re.M
+    )
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError('Unable to find __{meta}__ string.'.format(meta=meta))
+
+
+if __name__ == '__main__':
+    setup(
+        name=NAME,
+        description=find_meta('description'),
+        license=find_meta('license'),
+        url=find_meta('uri'),
+        version=find_meta('version'),
+        author=find_meta('author'),
+        author_email=find_meta('email'),
+        maintainer=find_meta('author'),
+        maintainer_email=find_meta('email'),
+        keywords=KEYWORDS,
+        long_description=read('README.md'),
+        packages=PACKAGES,
+        zip_safe=False,
+        classifiers=CLASSIFIERS,
+        install_requires=INSTALL_REQUIRES,
+        extras_require=EXTRAS_REQUIRE,
+    )
